821352eb07f0c5aae705b260dae8bfe6f8b4e4d7
Mike Pizzica <mike.pizzica@gmail.com>
2021-11-29 11:00:50 -0500

    add all view functions

--- main/index.js
+++ main/index.js
@@ -9,12 +9,21 @@ inquirer
       type: "list", // checkbox or list
       message: "What would you like to do?",
       name: "selection",
-      choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a row", "add an employee", "update an employee's role"]
+      choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee's role"]
     }
   ])
   .then((response) => {
     if(response.selection === "view all departments") {
       viewdepartments();
+    }
+    if(response.selection === "view all roles") {
+      viewroles();
+    }
+    if(response.selection === "view all employees") {
+      viewemployees();
+    }
+    if(response.selection === "add a department") {
+      adddepartment();
     }
 
   });
@@ -24,5 +33,34 @@ function viewdepartments(){
     if(err) throw err;
     console.table(data);
     // menu options function here
+  })
+}
+
+function viewroles(){
+  connection.query("SELECT * FROM role", function(err, data){
+    if(err) throw err;
+    console.table(data);
+    // menu options function here
+  })
+}
+
+function viewemployees(){
+  connection.query("SELECT * FROM employee", function(err, data){
+    if(err) throw err;
+    console.table(data);
+    // menu options function here
+  })
+}
+
+function adddepartment(){
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is the name of the dapartment?',
+      name: 'departmentname'
+    }
+  ])
+  .then((response) => {
+    console.log(response);
   })
 }
